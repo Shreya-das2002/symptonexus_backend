f9b96511e483854c04090a60c910e780a4ccb84e
Shreya-das2002 <das.shreya.sid@gmail.com>
2026-01-21 19:31:17 +0530

    up

--- models/Control_master.js
+++ models/Control_master.js
@@ -3,7 +3,7 @@ const sequelize = require("../config/database");
 
 const ControlMaster = sequelize.define("control_master", {
   control_master_id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
-  control_type: { type: DataTypes.ENUM("screen","button","action"), allowNull: false },
+  control_type: { type: DataTypes.ENUM("screen","button","action", "menu"), allowNull: false },
   control_key: { type: DataTypes.STRING, unique: true },
   control_name: DataTypes.STRING,
   control_desc: DataTypes.STRING,
