ad03ce1b14dade51a50661b13780d267ff07201a
Shreya-das2002 <das.shreya.sid@gmail.com>
2026-01-30 15:50:36 +0530

    update

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,8 +1,6 @@
 const AuthRoutes = require("./auth.route");
-const patientProfileRoutes = require("./patientProfile.route");
 
 
 module.exports = (app) => {
   app.use("/api/auth", AuthRoutes);
-  app.use("/api/patient", patientProfileRoutes);
 };
--- routes/patientProfile.route.js
+++
@@ -1,8 +0,0 @@
-const router = require("express").Router();
-const controller = require("../controllers/patientProfile.controller");
-const auth = require("../middlewares/auth.middleware");
-
-router.get("/profile", auth, controller.getProfile);
-router.post("/profile", auth, controller.saveProfile);
-
-module.exports = router;
