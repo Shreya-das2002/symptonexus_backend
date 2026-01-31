a855f0d09b72d739ec2a6e4f9e9c4ef78f615e8f
Shreya-das2002 <das.shreya.sid@gmail.com>
2026-01-31 11:45:04 +0530

    update

--- controllers/patientDetails.controller.js
+++ controllers/patientDetails.controller.js
@@ -19,7 +19,7 @@ class PatientDetailsController {
       }
 
       /* ================= SERVICE ================= */
-      // ✅ CALL METHOD, NOT THE INSTANCE
+      // CALL METHOD, NOT THE INSTANCE
       const { user, profile } =
         await patientProfileService.savePatientProfile(req.body);
 
